7cc15049a5eaed2f1d698bb394df42c38e9e6afb
Nikhil John <nikhiljohn1010@gmail.com>
2021-08-29 11:41:48 +0530

    moved worker code to package

--- CloudflareAPI/api/__init__.py
+++ CloudflareAPI/api/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
 from CloudflareAPI.api.user import User
-from CloudflareAPI.api.workers import Worker
+from CloudflareAPI.api.worker import Worker
 from CloudflareAPI.api.storage import Storage
 from CloudflareAPI.api.account import Account
